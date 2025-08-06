312020eaa49c6f0e167149c162b6bda5e54d2085
Linda Ngo <131126258+ling598@users.noreply.github.com>
2025-08-06 17:03:44 -0600

    Add files via upload

---
+++ android/src/csci3002/project/game/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package csci3002.project.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import csci3002.project.game.CafeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(CafeGame.getInstance(new Inventory()), config);
+	}
+}
